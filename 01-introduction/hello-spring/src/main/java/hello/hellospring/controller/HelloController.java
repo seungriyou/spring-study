d2214f9b9c2a7335e29f4a5895e2b1dc740e2a89
seungriyou <seungri0826@gmail.com>
2024-01-02 22:56:36 +0900

    feat(2): static content, mvc & template engine, and api

--- 01-introduction/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ 01-introduction/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,40 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    // <웹 개발 방법 #2> MVC와 템플릿 엔진 (변환된 HTML 파일을 내려준다.)
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    // <웹 개발 방법 #3> API (view 같은 것 없이 데이터만을 내려준다.)
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // response body에 이 데이터를 담아서 클라이언트에 보내겠다.
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    // static class로 만들면 class 안에서 또 class를 만들어서 사용할 수 있다.
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
