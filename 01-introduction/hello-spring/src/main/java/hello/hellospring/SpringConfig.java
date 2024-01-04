5a0a16d9df9e1e28849e2ec49a3518afd20edd80
seungriyou <seungri0826@gmail.com>
2024-01-04 23:09:48 +0900

    feat(6): migrate to jpa

--- 01-introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 01-introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,9 +1,9 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
-import javax.sql.DataSource;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +11,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    // DataSource는 application.properties에 적힌 값을 토대로 스프링 빈으로 생성 및 관리되기 때문에
-    // 마찬가지로 DI 해준다. (생성자 주입 + @Autowired)
-    private DataSource dataSource;
+    // Jdbc, JdbcTemplate에서는 DataSource를 DI 해주었으나,
+    // JPA에서는 EntityManager를 DI 해준다. (생성자 주입 + @Autowired)
+    // EntityManager는 DataSource와 마찬가지로 application.properties에 적힌 정보를 토대로 스프링 빈으로 생성 및 관리된다.
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -27,6 +28,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- 01-introduction/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ 01-introduction/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,11 +2,12 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
