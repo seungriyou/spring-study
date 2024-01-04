ed39956c430d883c635aee627360789078901546
seungriyou <seungri0826@gmail.com>
2024-01-04 22:32:34 +0900

    feat(6): migrate to jdbctemplate

--- 01-introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 01-introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,6 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import javax.sql.DataSource;
@@ -27,6 +27,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
