cca90fa63c42a78c99a6b1ae608587d2a03b7a0d
seungriyou <seungri0826@gmail.com>
2024-01-08 09:55:40 +0900

    feat(2): add order and discount domain

---
+++ 02-core_basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        // 1. 회원 정보 조회
+        Member member = memberRepository.findById(memberId);
+        // 2. 할인 정책
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+        // 3. 주문 생성
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
