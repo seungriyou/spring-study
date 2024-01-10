21c0d47329ed6d8767b919328d881145c72d36ce
seungriyou <seungri0826@gmail.com>
2024-01-10 23:25:42 +0900

    feat(7): apply lombok in constructor di

--- 02-core_basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 02-core_basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,22 +3,16 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     // DIP 준수: 인터페이스에만 의존
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    // 생성자 주입
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
