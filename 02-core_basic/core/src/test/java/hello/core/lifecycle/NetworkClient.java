ffd736991c4531821e75195ffade21be194cc4a6
seungriyou <seungri0826@gmail.com>
2024-01-12 11:01:07 +0900

    feat(8): (2) set `initMethod`, `destroyMethod` to `@Bean`

--- 02-core_basic/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ 02-core_basic/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -31,18 +28,16 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() throws Exception {
         // 의존관계 주입이 완료되면 호출
-        System.out.println("NetworkClient.afterPropertiesSet");
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() throws Exception {
         // 빈 종료 시 호출
-        System.out.println("NetworkClient.destroy");
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
