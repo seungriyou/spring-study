3f610a14a811c71b619ff95f6a7a47183460e4aa
seungriyou <seungri0826@gmail.com>
2024-01-13 10:02:55 +0900

    feat(9): prototype scope - problem w/ singleton

---
+++ 02-core_basic/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,78 @@
+package hello.core.scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(2);
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+        // ClientBean의 생성 후 의존관계 주입 시점에 프로토타입 스코프 빈의 주입이 완료 되어버리고, 계속 그것을 쓴다.
+        // 하지만 프로토타입 스코프 빈의 본래 의도는, 로직을 호출할 때마다 프로토타입을 항상 새로 생성해서 사용하려는 것이다!
+        private final PrototypeBean prototypeBean;
+
+        @Autowired  // @Autowired 생략해도 ok, lombok 이용하여 생성자 자체를 생략해도 ok
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
