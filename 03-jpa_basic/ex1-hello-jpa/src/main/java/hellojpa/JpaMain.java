02e963ad1ce96b1249562acb0eab74ff639b4e37
seungriyou <seungri0826@gmail.com>
2024-01-15 11:12:43 +0900

    feat(2): pagination w/ jpql

--- 03-jpa_basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 03-jpa_basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,11 +4,12 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         // EntityManagerFactory: 어플리케이션 로딩 시점에 딱 하나만
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // persistence.xml에서 읽어온다.
         // EntityManager: DB에 저장하거나 하는 트랜잭션 단위, DB 커넥션을 얻어서 동작해야 하는 경우마다
         EntityManager em = emf.createEntityManager();
 
@@ -17,10 +18,15 @@ public class JpaMain {
         tx.begin();
         try {
             // 수행하고 싶은 동작
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
-            // em.persist(findMember) 안 해도 된다!
+            // em.createQuery에서의 쿼리의 대상은 테이블이 아니라 객체가 된다. (m: Member 엔티티)
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
 
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
             // transaction 커밋
             tx.commit();
         } catch (Exception e) {
