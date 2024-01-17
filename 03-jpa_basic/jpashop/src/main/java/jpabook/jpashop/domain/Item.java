e7fcc96b0096ce7cecca97b251d4edbde0e57bdc
seungriyou <seungri0826@gmail.com>
2024-01-17 09:45:12 +0900

    feat(3): add `Delivery` and `Category` entities `Order`와 `Delivery`는 1:1 관계 (`@OneToOne`), `Item`과 `Category`는 N:M 관계 (`@ManyToMany`) `Category`는 `parent`, `child`로 셀프 N:1, 1:N 관계
    모두 양방향으로 구현

--- 03-jpa_basic/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
+++ 03-jpa_basic/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Item {
@@ -18,6 +21,9 @@ public class Item {
     private int price;
 
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
     public Long getId() {
         return id;
