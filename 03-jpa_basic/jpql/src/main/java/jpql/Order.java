eea00ba65d0252a082bec84cd2d033ec3fc728a7
seungriyou <seungri0826@gmail.com>
2024-01-19 12:08:38 +0900

    feat(10): add entities

---
+++ 03-jpa_basic/jpql/src/main/java/jpql/Order.java
@@ -0,0 +1,56 @@
+package jpql;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ORDERS") // 테이블 이름으로 ORDER를 사용하면 문제가 발생한다!
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int orderAmount;
+    @Embedded
+    private Address address;
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(int orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
