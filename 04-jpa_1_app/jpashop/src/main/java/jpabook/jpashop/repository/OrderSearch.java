918682e48b5bbcefad643c2797265204f851cbe0
seungriyou <seungri0826@gmail.com>
2024-01-21 11:10:11 +0900

    feat(6): dynamic query w/ jpql and criteria

---
+++ 04-jpa_1_app/jpashop/src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.OrderStatus;
+
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+}
