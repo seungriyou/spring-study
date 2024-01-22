33ab2e7fc05363e735a866c05ee1a64b0362d05b
seungriyou <seungri0826@gmail.com>
2024-01-22 14:59:18 +0900

    feat(7): add `Item` controller and view

---
+++ 04-jpa_1_app/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,83 @@
+package jpabook.jpashop.controller;
+
+import java.util.List;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+        // 더 좋은 설계: setter를 이용하지 않고, createBook과 같은 static 생성 메서드를 사용하는 것이
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+
+    @GetMapping("items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        // 예제 단순화를 위해 Book으로만 가정
+        Book item = (Book) itemService.findOne(itemId);
+
+        // 업데이트 시에 Book 엔티티가 아니라 폼을 이용한다.
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("items/{itemId}/edit")
+    public String updateItemForm(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
+        /* 다음과 같이 어설프게 엔티티를 반환하지 말자!
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+        itemService.saveItem(book);
+         */
+
+        // 다음과 같이 변경 대상이 되는 값이나 DTO를 만들어(ex. UpdateItemDto) 넘기는 방법을 사용한다.
+        itemService.updateItem(itemId, form.getPrice(), form.getName(), form.getStockQuantity());
+        return "redirect:/items";
+    }
+}
--- 04-jpa_1_app/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ 04-jpa_1_app/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -19,6 +19,17 @@ public class ItemService {
         itemRepository.save(item);
     }
 
+    @Transactional
+    public void updateItem(Long itemId, int price, String name, int stockQuantity) {
+        // findItem은 영속 상태이다!
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+        // 따라서 영속화하기 위한 작업을 따로 하지 않아도 된다.
+        // 트랜잭션이 완료되면 flush 되며, 그때 변경 감지로 인해 업데이트 쿼리가 날아간다.
+    }
+
     public List<Item> findItems() {
         return itemRepository.findAll();
     }
