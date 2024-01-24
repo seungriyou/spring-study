54b57d98e320afbd8e1ac177c347947cb00d868a
seungriyou <seungri0826@gmail.com>
2024-01-24 11:55:46 +0900

    feat(3): add pure jpa based repository

--- 06-spring_data_jpa/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ 06-spring_data_jpa/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -2,11 +2,15 @@ package study.datajpa.repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
 @Repository
 public class MemberJpaRepository {
+
+    // update용 메서드는 필요가 없다. 변경 감지 기능 때문이다!
 
     @PersistenceContext
     private EntityManager em;
@@ -14,6 +18,26 @@ public class MemberJpaRepository {
     public Member save(Member member) {
         em.persist(member);
         return member;
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        // JPQL 사용
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
 
     public Member find(Long id) {
--- 06-spring_data_jpa/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ 06-spring_data_jpa/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -2,14 +2,17 @@ package study.datajpa.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
 @SpringBootTest // 테스트 시에 스프링 빈 주입 받아야 하므로
 @Transactional  // 스프링에서 제공해주는 transactional 사용 권장 (JPA의 모든 데이터 변경은 트랜잭션 안에서 일어나야 함)
+@Rollback(value = false)
 class MemberJpaRepositoryTest {
 
     @Autowired
@@ -28,4 +31,37 @@ class MemberJpaRepositoryTest {
         assertThat(findMember).isEqualTo(member);   // 같은 영속성 컨텍스트(=> 트랜잭션) 내에서는 동일한 엔티티!
     }
 
+    @Test
+    public void basicCRUD() {
+        // 저장
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // 단건 조회
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 변경 감지 (업데이트)
+        // findMember1.setUsername("member1~~~~!");
+
+        // 리스트 조회
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 카운트
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deleteCount = memberJpaRepository.count();
+        assertThat(deleteCount).isEqualTo(0);
+
+    }
 }
