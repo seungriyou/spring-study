cd914b35e3174f4bfa77788d6a4513bd74ebf253
seungriyou <seungri0826@gmail.com>
2024-01-25 02:08:33 +0900

    feat(4): fetch join w/ `@EntityGraph` or JPQL

--- 06-spring_data_jpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ 06-spring_data_jpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -25,6 +25,7 @@ import lombok.ToString;
         name = "Member.findByUsername",
         query = "select m from Member m where m.username = :username"
 )
+//@NamedEntityGraph(name = "Member.all", attributeNodes = @NamedAttributeNode(("team")))
 public class Member {
 
     @Id
--- 06-spring_data_jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ 06-spring_data_jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -50,4 +51,24 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Modifying(clearAutomatically = true)  // .executeUpdate()
     @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
     int bulkAgePlus(@Param("age") int age);
+
+    // 페치 조인 (w/ JPQL)
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    // 페치 조인 (w/o JPQL) (w/ @EntityGraph)
+    // (1) 공통 메서드 오버라이드
+    @Override
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findAll();
+
+    // (2) JPQL + 엔티티 그래프
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    // (3) 메서드 이름으로 쿼리에서 특히 편리함
+    @EntityGraph(attributePaths = {"team"})
+//    @EntityGraph("Member.all")
+    List<Member> findEntityGraphByUsername(@Param("username") String username);
 }
