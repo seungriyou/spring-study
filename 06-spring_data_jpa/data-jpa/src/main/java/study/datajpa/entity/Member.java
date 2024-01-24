857c6f7c4986e221607ffcb6c3f5b0714a3ea06e
seungriyou <seungri0826@gmail.com>
2024-01-24 11:22:45 +0900

    feat(2): add `Member` and `Team` entities

--- 06-spring_data_jpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ 06-spring_data_jpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,26 +1,53 @@
 package study.datajpa.entity;
 
+import static jakarta.persistence.FetchType.LAZY;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+// JPA를 사용하므로 엔티티에는 기본 생성자가 있어야 한다. (public, protected 가능)
+@ToString(of = {"id", "username", "age"})   // "team"을 추가하면 양방향 연관관계로 무한루프
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    // JPA를 사용하므로 엔티티에는 기본 생성자가 있어야 한다. (public, protected 가능)
-    protected Member() {
-    }
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    // === 연관관계 편의 메서드 === //
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
