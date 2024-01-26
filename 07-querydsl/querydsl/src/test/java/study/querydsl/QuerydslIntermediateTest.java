c3d14aa41b03c024293944ea15351337aad7c5d3
seungriyou <seungri0826@gmail.com>
2024-01-27 02:13:00 +0900

    feat(4): sql function

--- 07-querydsl/querydsl/src/test/java/study/querydsl/QuerydslIntermediateTest.java
+++ 07-querydsl/querydsl/src/test/java/study/querydsl/QuerydslIntermediateTest.java
@@ -8,6 +8,7 @@ import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -270,5 +271,37 @@ public class QuerydslIntermediateTest {
                 .where(member.age.gt(18))
                 .execute();
         System.out.println("count = " + count);
+    }
+
+    // === SQL function === //
+    @Test
+    public void sqlFunction() {
+        List<String> result = queryFactory
+                .select(Expressions.stringTemplate(
+                        "function('replace', {0}, {1}, {2})",
+                        member.username, "member", "M"
+                ))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+
+    }
+
+    @Test
+    public void sqlFunction2() {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+//                .where(member.username.eq(
+//                        Expressions.stringTemplate("function('lower', {0})", member.username)))
+                .where(member.username.eq(member.username.lower()))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
     }
 }
