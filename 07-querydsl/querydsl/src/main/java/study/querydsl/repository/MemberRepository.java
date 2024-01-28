8b041f612c44efe500349a670cf0895433851e9a
seungriyou <seungri0826@gmail.com>
2024-01-28 11:16:27 +0900

    feat(7): `QuerydslPredicateExecutor`

--- 07-querydsl/querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
+++ 07-querydsl/querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
@@ -2,9 +2,10 @@ package study.querydsl.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
     // select m from Member m where m.username = ?
     List<Member> findByUsername(String username);
 }
--- 07-querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ 07-querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 @SpringBootTest
@@ -61,5 +62,33 @@ public class MemberRepositoryTest {
 
         List<MemberTeamDto> result = memberRepository.search(condition);
         assertThat(result).extracting("username").containsExactly("member4");
+    }
+
+    // === 스프링 데이터 JPA가 제공하는 Querydsl 기능 === //
+    @Test
+    public void querydslPredicateExecutorTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        // 곧바로 querydsl 조건을 넣을 수 있다.
+        Iterable<Member> result = memberRepository.findAll(
+                member.age.between(10, 40)
+                        .and(member.username.eq("member1"))
+        );
+        for (Member findMember : result) {
+            System.out.println("findMember = " + findMember);
+        }
     }
 }
