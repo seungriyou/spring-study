3e80e0bdf1e7e8db22deb3c2bc363322bbc8b7df
seungriyou <seungri0826@gmail.com>
2024-01-29 20:38:58 +0900

    init: create a project and add settings

---
+++ 09-security_jwt/spring-jwt/src/test/java/sr/security/springjwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package sr.security.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
