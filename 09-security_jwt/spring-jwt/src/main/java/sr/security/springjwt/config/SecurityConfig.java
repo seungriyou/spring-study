6a919b646a36818d88bdfb479a2aebe2bf01ba60
seungriyou <seungri0826@gmail.com>
2024-01-30 14:58:40 +0900

    feat(13): add cors setting

--- 09-security_jwt/spring-jwt/src/main/java/sr/security/springjwt/config/SecurityConfig.java
+++ 09-security_jwt/spring-jwt/src/main/java/sr/security/springjwt/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package sr.security.springjwt.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import sr.security.springjwt.jwt.JwtFilter;
 import sr.security.springjwt.jwt.JwtUtil;
 import sr.security.springjwt.jwt.LoginFilter;
@@ -40,6 +44,26 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        // cors
+        http
+                .cors((corsCustomizer -> corsCustomizer
+                        .configurationSource(new CorsConfigurationSource() {
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                                CorsConfiguration configuration = new CorsConfiguration();
+
+                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                                configuration.setAllowedMethods(Collections.singletonList("*"));
+                                configuration.setAllowCredentials(true);
+                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                                configuration.setMaxAge(3600L);
+
+                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                                return configuration;
+                            }
+                        })));
+
         // csrf disable
         // 세션 방식에서는 세션이 고정되므로 csrf 공격에 방어해야 하지만, JWT 방식에서는 세션은 stateless로 관리하므로 방어하지 않아도 ok
         http
@@ -61,7 +85,7 @@ public class SecurityConfig {
         // JWT 검증 필터 등록 (LoginFilter 앞에)
         http
                 .addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
-        
+
         // 커스텀 로그인 필터 등록 (UsernamePasswordAuthenticationFilter 대체해서 등록할 것이므로 addFilterAt 사용)
         http
                 .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
