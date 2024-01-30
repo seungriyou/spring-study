373615a9bc762a4c433638803454e912370c28e5
seungriyou <seungri0826@gmail.com>
2024-01-30 14:51:10 +0900

    feat(12): get session information

--- 09-security_jwt/spring-jwt/src/main/java/sr/security/springjwt/controller/MainController.java
+++ 09-security_jwt/spring-jwt/src/main/java/sr/security/springjwt/controller/MainController.java
@@ -1,5 +1,10 @@
 package sr.security.springjwt.controller;
 
+import java.util.Collection;
+import java.util.Iterator;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +14,17 @@ public class MainController {
     @GetMapping("/")
     public String mainp() {
 
-        return "main controller";
+        // 세션 현재 사용자 username 가져오기
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        // 세션 현재 사용자 role 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "main controller" + username + role;
     }
 }
